59ec1fb12658308c677215903c1a4bf823bb3c72
Ijelu Peter <ijelupeter19@gmail.com>
2026-09-24 15:03:58 +0200

    Create SQL analysis file

---
+++ sql/financial_sector_analysis.sql
@@ -0,0 +1,40 @@
+-- ============================================================
+-- DUTCH FINANCIAL SECTOR ANALYSIS
+-- Data Source: Statistics Netherlands (CBS)
+-- Dataset: Financial balance sheets and transactions by sectors
+-- ============================================================
+
+
+-- ============================================================
+-- 1. DATA EXPLORATION
+-- ============================================================
+
+-- Queries will be added here as the dataset is explored.
+
+
+-- ============================================================
+-- 2. DATA CLEANING
+-- ============================================================
+
+-- Data cleaning queries will be added here.
+
+
+-- ============================================================
+-- 3. FINANCIAL SECTOR ANALYSIS
+-- ============================================================
+
+-- Analysis queries will be added here.
+
+
+-- ============================================================
+-- 4. TREND ANALYSIS
+-- ============================================================
+
+-- Time-series analysis will be added here.
+
+
+-- ============================================================
+-- 5. BUSINESS INSIGHTS
+-- ============================================================
+
+-- Final analytical queries and findings will be added here.
